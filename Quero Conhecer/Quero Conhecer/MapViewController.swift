2579112edfa834f4a8d49e9fa60df6e93598e27c
vloss <viniciusvianaloss@gmail.com>
2023-02-06 18:44:33 -0300

    Adicionado pontos no mapa com Annotation.

--- Quero Conhecer/Quero Conhecer/MapViewController.swift
+++ Quero Conhecer/Quero Conhecer/MapViewController.swift
@@ -14,12 +14,38 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var viInfo: UIView!
     @IBOutlet weak var lbName: UILabel!
-    
     @IBOutlet weak var lbAddress: UILabel!
+    
+    var places: [Place]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        searchBar.isHidden  = true
+        viInfo.isHidden     = true
+        
+        if places.count == 1 {
+            title = places[0].name
+        } else {
+            title = "Meus Lugares"
+        }
+        
+        for place in places {
+            addToMap(place)
+        }
+        
+        showPlaces()
+    }
+    
+    func addToMap (_ place: Place){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = place.coordinate
+        annotation.title = place.name
+        mapView.addAnnotation(annotation)
+    }
+    
+    func showPlaces(){
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
     
     @IBAction func showRoute(_ sender: UIButton) {
--- Quero Conhecer/Quero Conhecer/PlacesTableViewController.swift
+++ Quero Conhecer/Quero Conhecer/PlacesTableViewController.swift
@@ -11,17 +11,32 @@ class PlacesTableViewController: UITableViewController {
     
     var places: [Place] = []
     let ud = UserDefaults.standard
+    var lbNoPlaces: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loadPlaces()
+        
+        lbNoPlaces = UILabel()
+        lbNoPlaces.text = "Cadastre os locais que deseja conhecer\nclicando no botão + acima."
+        lbNoPlaces.textAlignment = .center
+        lbNoPlaces.numberOfLines = 0
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier! != "mapSegue" {
             let vc = segue.destination as! PlaceFinderViewController
             vc.delegate = self
+        } else {
+            let vc = segue.destination as! MapViewController
+            
+            switch sender {
+            case let place as Place:
+                vc.places = [place]
+            default:
+                vc.places = places
+            }
         }
     }
     
@@ -44,11 +59,27 @@ class PlacesTableViewController: UITableViewController {
         let json = try? JSONEncoder().encode(places)
         ud.set(json, forKey: "places")
     }
+    
+    @objc func showAll(){
+        performSegue(withIdentifier: "mapSegue", sender: nil)
+    }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
+        // Adicionando botão na navegation bar através do código fonte.
+        if places.count > 0 {
+            let btShowAll = UIBarButtonItem(title: "Mostrar todos no Mapa", style: .plain, target: self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = btShowAll
+            tableView.backgroundView = nil
+        } else {
+            navigationItem.leftBarButtonItem = nil
+            
+            tableView.backgroundView = lbNoPlaces
+        }
+        
+        
         return places.count
     }
 
@@ -61,50 +92,18 @@ class PlacesTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let place = places[indexPath.row]
+        performSegue(withIdentifier: "mapSegue", sender: place)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            places.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            savePlaces()
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
